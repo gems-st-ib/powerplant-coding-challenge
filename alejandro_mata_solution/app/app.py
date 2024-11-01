b72222231dc941ad157b9b8b21b6d55b47791d5a
Alejandro Mata <alemalvarez@icloud.com>
2024-11-01 12:48:19 +0100

    Change port to requirement & activate productionplan post endpoint

--- alejandro_mata_solution/app/app.py
+++ alejandro_mata_solution/app/app.py
@@ -1,7 +1,12 @@
-from flask import Flask
+from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello a!' 
+    return 'Testing endpoint', 200 
+
+@app.route('/productionplan', methods=['POST'])
+def production_plan():
+    payload = request.get_json()
+    return jsonify(payload), 200
